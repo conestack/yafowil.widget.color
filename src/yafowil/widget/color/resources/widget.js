942a2f7aa609da2928063cb5d95a178de2cd1333
Robert Niederreiter <office@squarewave.at>
2021-10-28 07:11:47 +0200

    Base work WIP

--- src/yafowil/widget/color/resources/widget.js
+++ src/yafowil/widget/color/resources/widget.js
@@ -1,19 +1,28 @@
-(function (exports, $) {
+(function (exports, $, iro) {
     'use strict';
 
     class ColorWidget {
         static initialize(context) {
-            $('div.color-picker-trigger', context).each(function() {
+            $('input.color-picker', context).each(function() {
                 new ColorWidget($(this));
             });
         }
         constructor(elem) {
+            this.picker = null;
+            this.picker_elem = null;
             this.elem = elem;
+            let color_elem = this.color_elem = $('<span class="color-picker-color" />');
+            elem.after(color_elem);
             this.trigger_handle = this.trigger_handle.bind(this);
-            elem.on('click', this.trigger_handle);
+            elem.on('focus', this.trigger_handle);
+            color_elem.on('click', this.trigger_handle);
         }
         trigger_handle(evt) {
             evt.preventDefault();
+            let picker_elem = this.picker_elem = $('<div class="color-picker-wrapper" />');
+            this.color_elem.after(picker_elem);
+            this.picker = new iro.ColorPicker(picker_elem.get(0), {
+            });
         }
     }
 
@@ -38,5 +47,5 @@
 
     return exports;
 
-})({}, jQuery);
+})({}, jQuery, iro);
 //# sourceMappingURL=widget.js.map
