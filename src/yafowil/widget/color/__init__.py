674c85e84601d7f04ca22338d5a9307a688a4e3d
Robert Niederreiter <rnix@squarewave.at>
2022-05-20 16:29:20 +0200

    "Register resources with webresource"

--- src/yafowil/widget/color/__init__.py
+++ src/yafowil/widget/color/__init__.py
@@ -1,9 +1,49 @@
 from yafowil.base import factory
 from yafowil.utils import entry_point
 import os
+import webresource as wr
 
 
-resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
+resources_dir = os.path.join(os.path.dirname(__file__), 'resources')
+
+
+##############################################################################
+# Default
+##############################################################################
+
+# webresource ################################################################
+
+scripts = wr.ResourceGroup(name='scripts')
+scripts.add(wr.ScriptResource(
+    name='iro-js',
+    # actually it not depends on jquery, but yafowil-color-js does
+    # think about multiple depends values in webresource
+    depends='jquery-js',
+    directory=os.path.join(resources_dir, 'iro'),
+    resource='iro.js',
+    compressed='iro.min.js'
+))
+scripts.add(wr.ScriptResource(
+    name='yafowil-color-js',
+    depends='iro-js',
+    directory=resources_dir,
+    resource='widget.js',
+    compressed='widget.min.js'
+))
+
+styles = wr.ResourceGroup(name='styles')
+styles.add(wr.StyleResource(
+    name='yafowil-color-css',
+    directory=resources_dir,
+    resource='widget.css'
+))
+
+resources = wr.ResourceGroup(name='color-resources')
+resources.add(scripts)
+resources.add(styles)
+
+# B/C resources ##############################################################
+
 js = [{
     'group': 'yafowil.widget.color.dependencies',
     'resource': 'iro/iro.js',
@@ -13,15 +53,23 @@ js = [{
     'resource': 'widget.js',
     'order': 21,
 }]
-default_css = [{
+css = [{
     'group': 'yafowil.widget.color.common',
     'resource': 'widget.css',
     'order': 21,
 }]
 
 
+##############################################################################
+# Registration
+##############################################################################
+
 @entry_point(order=10)
 def register():
     from yafowil.widget.color import widget  # noqa
-    factory.register_theme('default', 'yafowil.widget.color',
-                           resourcedir, js=js, css=default_css)
+
+    # Default
+    factory.register_theme(
+        'default', 'yafowil.widget.color', resources_dir,
+        js=js, css=css, resources=resources
+    )
