8e47b81de383d4ff68e3f027302dba9f50086fec
Lena Daxenbichler <lena.daxenbichler@r2solutions.at>
2023-03-27 11:21:45 +0200

    translations

--- src/yafowil/widget/color/widget.py
+++ src/yafowil/widget/color/widget.py
@@ -23,7 +23,6 @@ def color_extractor(widget, data):
         return extracted
     format = attr_value('format', widget, data)
 
-    err_base = _('incorrect_color_string', default='Incorrect Color String: ')
     formats = ', '.join(
         [
             'hexString',
@@ -40,19 +39,22 @@ def color_extractor(widget, data):
         if (not extracted.startswith('#')):
             msg = _(
                 'unknown_color_format',
-                    default=(
-                        'Unknown Format, supported formats: ${formats}'
-                    ),
-                    mapping={
-                        'formats':formats
-                    }
-                )
+                default=u'Unknown Format. Supported formats: ${formats}',
+                mapping={'formats':formats}
+            )
             raise ExtractionError(msg)
-
-        if format == 'hexString' and len(extracted) != 7:
-            raise ExtractionError('Incorrect hex Color String length: string length must be 7')
+        elif format == 'hexString' and len(extracted) != 7:
+            msg = _(
+                'incorrect_hex_length',
+                default=u'Incorrect hex Color String length: string length must be 7'
+            )
+            raise ExtractionError(msg)
         elif format == 'hex8String' and len(extracted) != 9:
-            raise ExtractionError('Incorrect hex alpha Color String length: string length must be 9')
+            msg = _(
+                'incorrect_hex8_length',
+                default=u'Incorrect hex8 Color String length: string length must be 9'
+            )
+            raise ExtractionError(msg)
         color = extracted[1:]
         r = color[0:2]
         g = color[2:4]
@@ -67,33 +69,52 @@ def color_extractor(widget, data):
                 a = int(a, 16)
         except Exception as e:
             args = ', '.join(e.args)
-            raise ExtractionError(u'Incorrect Hex Value: {}'.format(args))
+            msg = _(
+                'incorrect_hex_value',
+                default=u'Incorrect Hex Value: ${args}',
+                mapping={'args':args}
+            )
+            raise ExtractionError(msg)
     elif format == 'hslString' or format == 'hslaString':
         hsl_format = 'hsl([0-360], [0-100]%, [0-100]%)'
         hsla_format = 'hsla([0-360], [0-100]%, [0-100]%, [0-1])'
 
         if not extracted.endswith(')'):
-            raise ExtractionError("{} Unclosed '('".format(err_base))
+            msg = _(
+                'unclosed_brace',
+                default=u"Incorrect Color String:  Unclosed bracket."
+            )
+            raise ExtractionError(msg)
         elif format == 'hslString' and not extracted.startswith('hsl('):
-            raise ExtractionError(
-                "{} String must start with 'hsl('".format(err_base)
+            msg = _(
+                'hsl_str_start',
+                default=u"Incorrect Color String: String must start with 'hsl'"
             )
+            raise ExtractionError(msg)
         elif format == 'hslaString' and not extracted.startswith('hsla('):
-            raise ExtractionError(
-                "{} String must start with 'hsla('".format(err_base)
+            msg = _(
+                'hsla_str_start',
+                default=u"Incorrect Color String: String must start with 'hsla'"
             )
+            raise ExtractionError(msg)
         length = 3 if format == 'hslString' else 4
         color = extracted[length + 1:-1]
         color = [channel.strip() for channel in color.split(',')]
 
         if format == 'hslString' and len(color) != 3:
-            raise ExtractionError(
-                '{} expected format: {}'.format(err_base, hsl_format)
+            msg = _(
+                'hsl_str_length',
+                default=u"Incorrect Color String: expected format: ${hsl_format}",
+                mapping={'hsl_format':hsl_format}
             )
+            raise ExtractionError(msg)
         if format == 'hslaString' and len(color) != 4:
-            raise ExtractionError(
-                '{} expected format: {}'.format(err_base, hsla_format)
+            msg = _(
+                'hsla_str_length',
+                default=u"Incorrect Color String: expected format: ${hsla_format}",
+                mapping={'hsl_format':hsla_format}
             )
+            raise ExtractionError(msg)
 
         h = color[0]
         s = color[1]
@@ -101,75 +122,120 @@ def color_extractor(widget, data):
         a = color[3] if format == 'hslaString' else False
 
         if int(h) < 0 or int(h) > 360:
-            raise ExtractionError(
-                '{} value for hue must be between 0 and 360.'.format(err_base))
+            msg = _(
+                "incorrect_hue_value",
+                default=u"Incorrect Color String: value for hue must be between 0 and 360."
+            )
+            raise ExtractionError(msg)
         elif not s.endswith('%') or int(s[0:-1]) not in range(0, 101):
-            raise ExtractionError(
-                '{} value for saturation must be between 0 and 100 followed by "%".'
-                .format(err_base)
+            msg = _(
+                "incorrect_saturation_value",
+                default=u'Incorrect Color String: value for saturation must be between 0 and 100 followed by "%".'
             )
+            raise ExtractionError(msg)
         elif not l.endswith('%') or int(l[0:-1]) not in range(0, 101):
-            raise ExtractionError(
-                '{} value for lightness must be between 0 and 100 followed by "%".'
-                .format(err_base)
+            msg = _(
+                "incorrect_lightness_value",
+                default=u'Incorrect Color String: value for lightness must be between 0 and 100 followed by "%".'
             )
+            raise ExtractionError(msg)
         elif a and float(a) < 0 or float(a) > 1:
-            raise ExtractionError(
-                '{} Alpha value must be between 0 and 1.'.format(err_base)
+            msg = _(
+                "incorrect_alpha_value",
+                default=u'Incorrect Color String: Alpha value must be between 0 and 1.'
             )
+            raise ExtractionError(msg)
     elif format == 'rgbString' or format == 'rgbaString':
         rgb_format = 'rgb([0-255], [0-255], [0-255])'
         rgba_format = 'rgba([0-255], [0-255], [0-255], [0-1])'
 
         if not extracted.endswith(')'):
-            raise ExtractionError("{} Unclosed '('".format(err_base))
+            msg = _(
+                'unclosed_brace',
+                default=u"Incorrect Color String: Unclosed bracket."
+            )
+            raise ExtractionError(msg)
         elif format == 'rgbString' and not extracted.startswith('rgb('):
-            raise ExtractionError(
-                "{} String must start with 'rgb('".format(err_base)
+            msg = _(
+                'rgb_str_start',
+                default=u"Incorrect Color String: String must start with 'rgb'"
             )
+            raise ExtractionError(msg)
         elif format == 'rgbaString' and not extracted.startswith('rgba('):
-            raise ExtractionError(
-                "{} String must start with 'rgba('".format(err_base)
+            msg = _(
+                'rgba_str_start',
+                default=u"Incorrect Color String: String must start with 'rgba'"
             )
+            raise ExtractionError(msg)
         length = 3 if format == 'rgbString' else 4
         color = extracted[length + 1:-1]
         color = [channel.strip() for channel in color.split(',')]
 
         if format == 'rgbString' and len(color) != 3:
-            raise ExtractionError(
-                '{} expected format: {}'.format(err_base, rgb_format)
+            msg = _(
+                'rgb_str_length',
+                default=u"Incorrect Color String: expected format: ${rgb_format}",
+                mapping={'rgb_format':rgb_format}
             )
+            raise ExtractionError(msg)
         elif format == 'rgbaString' and len(color) != 4:
-            raise ExtractionError(
-                '{} expected format: {}'.format(err_base, rgba_format)
+            msg = _(
+                'rgba_str_length',
+                default=u"Incorrect Color String: expected format: ${rgba_format}",
+                mapping={'rgba_format':rgba_format}
             )
+            raise ExtractionError(msg)
         r = color[0]
         g = color[1]
         b = color[2]
         a = color[3] if format == 'rgbaString' else False
 
         if int(r) < 0 or int(r) > 255:
-            raise ExtractionError(
-                '{} value for red must be between 0 and 255.'.format(err_base))
-        elif int(g) < 0 or int(g) > 255:
-            raise ExtractionError(
-                '{} value for green must be between 0 and 255.'.format(err_base))
-        elif int(b) < 0 or int(b) > 255:
-            raise ExtractionError(
-                '{} value for blue must be between 0 and 255.'.format(err_base))
-        elif a and float(a) < 0 or float(a) > 1:
-            raise ExtractionError(
-                '{} Alpha value must be between 0 and 1.'.format(err_base)
+            msg = _(
+                'incorrect_red_value',
+                default=u"Incorrect Color String: value for red must be between 0 and 255."
             )
+            raise ExtractionError(msg)
+        elif int(g) < 0 or int(g) > 255:
+            msg = _(
+                'incorrect_green_value',
+                default=u"Incorrect Color String: value for green must be between 0 and 255."
+            )
+            raise ExtractionError(msg)
+        elif int(b) < 0 or int(b) > 255:
+            msg = _(
+                'incorrect_blue_value',
+                default=u"Incorrect Color String: value for blue must be between 0 and 255."
+            )
+            raise ExtractionError(msg)
+        elif a and float(a) < 0 or float(a) > 1:
+            msg = _(
+                "incorrect_alpha_value",
+                default=u'Incorrect Color String: Alpha value must be between 0 and 1.'
+            )
+            raise ExtractionError(msg)
     elif format == 'kelvin':
         try:
             color = int(extracted)
         except ValueError:
-            raise ExtractionError('Unknown Format, expected format: int(1000 - 40000) or str(1000 - 40000)')
+            msg = _(
+                "incorrect_kelvin_format",
+                default=u'Unknown Format, expected format: int(1000 - 40000) or str(1000 - 40000)'
+            )
+            raise ExtractionError(msg)
         if color < 1000 or color > 40000:
-            raise ExtractionError('Kelvin Temperature out of range (1000-40000)')
+            msg = _(
+                "incorrect_kelvin_number",
+                default=u'Kelvin Temperature out of range (1000-40000)'
+            )
+            raise ExtractionError(msg)
     else:
-        raise ExtractionError('Unknown Format')
+        msg = _(
+            'unknown_color_format',
+            default=u'Unknown Format. Supported formats: ${formats}',
+            mapping={'formats':formats}
+        )
+        raise ExtractionError(msg)
     return extracted
 
 
